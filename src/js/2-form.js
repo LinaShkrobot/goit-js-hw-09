acde72eb986a95d1dadf030af22ae9733b42ded5
Lina <linaskrobot@gmail.com>
2025-09-06 22:58:26 +0000

    my ninth hw-js

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageTextarea = form.elements.message;
+
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('submit', handlerSubmit);
+
+const feedbackFormStateJson = localStorage.getItem(localStorageKey);
+
+if (feedbackFormStateJson) {
+  const feedbackFromState = JSON.parse(feedbackFormStateJson);
+  emailInput.value = feedbackFromState.email;
+  messageTextarea.value = feedbackFromState.message;
+  formData.email = feedbackFromState.email;
+  formData.message = feedbackFromState.message;
+}
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    event.target.reset();
+    localStorage.removeItem(localStorage, localStorageKey);
+    form.reset();
+  }
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
